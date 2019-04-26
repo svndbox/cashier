938a00a6dad879f0a532a2c9e3fbd72ae2b51125
orpheus <ryanachacon@gmail.com>
2019-04-26 07:57:12 -0700

    push last changes

--- plaid.config.js
+++ plaid.config.js
@@ -1,3 +1,17 @@
+import PlaidAuthenticator from "react-native-plaid-link";
+import React from "react";
+
 process.env.PLAID_CLIENT_ID = '5b7ce29c069553001133a645';
 process.env.PLAID_SECRET = 'f426e4cc5953bbc2d04b5d0a317c25';
 process.env.PLAID_PUBLIC_KEY = 'a29ea56e57913cfcc818011b31c617';
+
+
+{/*<PlaidAuthenticator*/}
+	{/*onMessage={this.onMessage}*/}
+	{/*publicKey="eecc6d6382543dbee6478afbc5879b"*/}
+	{/*env="sandbox"*/}
+	{/*product="auth,transactions"*/}
+	{/*onLoad={this.onLoad}*/}
+	{/*onLoadStart={this.onLoadStart}*/}
+	{/*onLoadEnd={this.onLoadEnd}*/}
+{/*/>*/}
